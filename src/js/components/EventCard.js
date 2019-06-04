516fb6e1a6918b609132a902b5948f5d824290f4
Nathan Lafrance Berger <nathan.lafranceb@gmail.com>
2019-06-04 16:00:38 -0400

    RImproved child constrraints in event card. Made date presentation human readable

--- src/js/components/EventCard.js
+++ src/js/components/EventCard.js
@@ -5,17 +5,41 @@ export default class EventCard extends Component {
 
   constructor() {
     super()
+    this.colors = {
+      "Fitness": "#f352a3",
+      "Environmental": "#67d414",
+      "Culture": "#46648f",
+      "Entertainment": '#52c3f4',
+      "Food&Drink": "#fdd501",
+    }
+
+    this.readableTime = this.readableTime.bind(this)
+    this.readableTimestamp = this.readableTimestamp.bind(this)
+  }
+
+  readableTime = (time) => {
+    if (time.charAt(0) == '0')
+      var t = time.substr(1)
+    var t = t.slice(0,-1)
+    return t
+  }
+
+  readableTimestamp = (ts) => {
+    var date = new Date(ts)
+    //return `${date.getHours()}:${date.getMinutes()} - ${date.getDay()}, ${date.getMonth()} ${date.getDate()} ${date.getFullYear()}`
+    return date.toString().substring(0, 21)
   }
 
   render() {
     return(
         <div className="card">
+          <div className="card-catband" style={ {backgroundColor: this.colors[this.props.event.category]} }/>
           <img className="card-image" src={this.props.event.photo}/>
           <div className="card-info-wrapper">
             <p className="card-title"> {this.props.event.title} </p>
             <p className="card-description"> {this.props.event.description} </p>
-            <p className="card-date"> Start: {this.props.event.start}</p>
-            <p className="card-date"> End: {this.props.event.end}</p>
+            <p className="card-date-start"> Start: {this.readableTimestamp(this.props.event.start)}</p>
+            <p className="card-date-end"> End: {this.readableTimestamp(this.props.event.end)}</p>
           </div>
         </div>
     )
